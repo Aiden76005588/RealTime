6bb42c718bdb965c408ab4da701c8f2f49a6e4eb
Aiden <66179677+Aiden76005588@users.noreply.github.com>
2021-03-20 16:34:36 +0900

    [DB] MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const fruits = require("./data/fruits");
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://hyunkyu:<hyunkyu1234>@cluster0.vpbup.mongodb.net/mernfruits?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection
+db.once('open',()=>console.log('Connected to DB')) 
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World");
